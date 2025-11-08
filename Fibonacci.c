dd127967c647c49b54552ab9231cac914d70023d
Lakshay045 <lakshaymittal709@gmail.com>
2025-11-08 22:40:15 +0530

    Add files via upload

---
+++ Fibonacci.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num ;
+
+    printf("Enter the Number : ");
+    scanf("%d",&num);
+
+    int a = -1 ,b = 1 ,c;
+
+    for(int i=1 ; i <= num;i++){
+        c = a + b ;
+        printf("%d",c);
+        a = b;
+        b = c;
+
+        printf("\n");
+
+    }
+    return 0;
+}
